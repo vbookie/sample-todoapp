357dec3f7a0a33be04cd65fdeebfd465446c2beb
bookie <vbookie@gmail.com>
2017-04-17 15:50:37 +0300

    Added some stylings

--- webapp/src/app/tasks/task-item.component.ts
+++ webapp/src/app/tasks/task-item.component.ts
@@ -3,7 +3,8 @@ import { TaskService, Task } from './'
 
 @Component({
     selector: 'task-item',
-    templateUrl: 'task-item.component.html'
+    templateUrl: 'task-item.component.html',
+    styleUrls: ['task-item.component.css']
 })
 
 export class TaskItemComponent implements OnInit {
